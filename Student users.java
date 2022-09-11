839adf4b2882a02e25b558954ea4d75ebba55836
MahidharNagolu <112749213+MahidharNagolu@users.noreply.github.com>
2022-09-11 19:51:11 +0530

    Create Student users.java
    Test Cases:
    1. Total User: 0
    2. Total User: -143
    3. Total User: 1026, Staff User: 1026
    4. Total User: 450, Staff User: 540
    5. Total User: 600, Staff User: 450

---
+++ Student users.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+                 
+
+public class JavaProgram
+
+{
+
+    public static void main(String args[])
+
+  {
+
+    int first, second, subtract;
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter Total no of staff uers : ");
+
+    first = scanner.nextInt();
+
+    System.out.print("Enter Total no of sudent users : ");
+
+    second = scanner.nextInt();
+
+    subtract = first - second;
+
+    System.out.println("Difference = " + subtract);
+
+  }
+
+}
